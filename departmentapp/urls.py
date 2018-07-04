f0b31f09c2c0d4e0321ea753983a85a2e36ba7ca
Dotel <saramsha.d@gmail.com>
2018-07-04 19:07:42 +0545

    Before changing urls

--- departmentapp/urls.py
+++ departmentapp/urls.py
@@ -5,5 +5,9 @@ app_name = 'departmentapp'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<dept_code>/',views.dashboard, name='dashboard')
+    path('<dept_id>/',views.dashboard, name='dashboard'),
+    path('<dept_id>/academic',views.academic,name='academic'),
+    path('<dept_id>/nonacademic', views.nonacademic, name='nonacademic'),
+    path('<dept_id>/course', views.course, name='course'),
+
 ]
--- departmentapp/views.py
+++ departmentapp/views.py
@@ -10,8 +10,8 @@ def home(request):
     print(result)
     return render(request,'departmentapp/home.html',{'result':result})
 
-def dashboard(request,dept_code):
-    result = tables.department.get("dept_code = '{}'".format(dept_code))
+def dashboard(request,dept_id):
+    result = tables.department.get("dept_id = '{}'".format(dept_id))
     if not result['error']:
         # try catch block check if empty record is returned through indexing operation
         try:
@@ -40,7 +40,7 @@ def dashboard(request,dept_code):
             #   To get the HOD details in a dictionary field 'hod'
             hod_details = tables.get(tables.join('employee','academic'),'salutation,staff_fname,staff_lname,email,mobile_contact,photo_url',
                                      condition='department_id = {} and post_id = {}'.format(result['department']['dept_id'],1))
-            if(hod_details['error']):
+            if(hod_details['error'] or hod_details['rows'] == [] ):
                 raise Exception('HOD Details cannot be retrieved')
             result['hod'] = hod_details['rows'][0]  # Since, only one HOD in each dept, index 0 will only be filled in result
 
@@ -49,7 +49,7 @@ def dashboard(request,dept_code):
                                      'salutation,staff_fname,staff_lname,email,mobile_contact,photo_url',
                                      condition='department_id = {} and post_id = {}'.format(
                                          result['department']['dept_id'], 2))
-            if (dhod_details['error']):
+            if (dhod_details['error'] or dhod_details['rows'] == []):
                 raise Exception('Deputy HOD Details cannot be retrieved')
             result['dhod'] = dhod_details['rows']  # Since, more than one HOD is possible in a dept, no indexing
 
@@ -58,37 +58,19 @@ def dashboard(request,dept_code):
             result = {'error': True, 'message': 'No such department is enlisted in the database'}
 
         except Exception as e:
-            result = {'error': True, 'message': 'There was an error in querying the database: {}'.format(str(e))}
+            result = {'error': True, 'department': result['department'], 'message': 'There was an error in querying the database: {}'.format(str(e))}
 
     return render(request,'departmentapp/dashboard.html',{'result':result})
 
-def test(request):
-    test = tables.employee.insert(9999,'David','','Seaman',None,None,7787,8878,None,1)
-    print(test)
-    return render(request,'departmentapp/test.html')
+def academic(request,dept_code):
+    department_info = tables.department.get('')
+    column_names = 'staff_id,salutation,staff_fname,staff_mname,staff_lname,email,mobile_contact,designation'
+    result = tables.get(tables.join('academic','employee'),column_names,)
+    return render(request,'departmentapp/academic.html')
 
-    # with connection.cursor() as cursor:
-    #     query1 = '''SELECT * FROM course;'''
-    #
-    #     try:
-    #         cursor.execute(query1)
-    #         results = cursor.fetchall()
-    #         rows = []
-    #         columns = [each[0] for each in cursor.description]
-    #         print(columns)
-    #         for result in results:
-    #             row = dict(zip(columns,result))
-    #             rows.append(row)
-    #         print(rows)
-    #         return render(request,'departmentapp/test.html',{'results':rows})
-    #
-    #     except Exception as e:
-    #         print('Couldnt fetch data due to error ... '+ str(e))
+def nonacademic(request,dept_code):
+    return render(request,'departmentapp/nonacademic.html')
 
-    # query2 = '''SELECT * FROM academic WHERE staff_id='%s' '''%('9999')
-        #
-        # cursor.execute(query2)
-        # results = cursor.fetchall()
-        # print(results)
-        # for result in results:
-        #     print(result==None)
+def course(request,dept_code):
+    return render(request,'departmentapp/course.html')
+
