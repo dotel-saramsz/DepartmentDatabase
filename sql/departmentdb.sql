2563e32a18310f3883e555b5e6954bddaefeeacc
Dotel <saramsha.d@gmail.com>
2018-08-03 02:56:21 +0545

    Sorted display added

--- sql/departmentdb.sql
+++ sql/departmentdb.sql
@@ -557,4 +557,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-08-03  2:53:04
+-- Dump completed on 2018-08-03  2:55:19
